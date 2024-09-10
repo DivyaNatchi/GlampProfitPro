05ed494b2f58cdfdb4afa228343da9775706c1c0
Divyalakshmi <ndivyalakshmi88@gmail.com>
2024-09-10 22:45:50 +0300

    collapse navbar, footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,15 @@
-export default function Header() {
-  return <h1 className="footer">Footer section</h1>;
+import "../styles/footer.css";
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <h1 className="footer">
+      <div className="row">
+        <div className="col-12"></div>
+        <p className="copyrightText">
+          Copyright © 1999 - {currentYear} Wigwam Holidays Ltd | All rights
+          reserved
+        </p>
+      </div>
+    </h1>
+  );
 }
