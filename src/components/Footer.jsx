e2cf8f96c18078f549750e0a82426fd7e57e02cd
Divyalakshmi <ndivyalakshmi88@gmail.com>
2024-09-15 10:02:15 +0300

    changes in footer and main content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import "../styles/footer.css";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <h1 className="footer mt-5">
+    <h1 className="footer">
       <div className="row">
         <div className="col-12"></div>
         {/* <p className="copyrightText">
